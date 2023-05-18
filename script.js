3e59898d60c66394df24a74b8937370aae284813
sai vikas <saivikas461@gmail.com>
2023-05-18 20:52:54 +0530

    added details input box

--- script.js
+++ script.js
@@ -9,7 +9,10 @@ const dish = document.querySelectorAll('.dish');
 const add = document.querySelectorAll('.add');
 const cart_list = document.querySelector('.cart_list')
 const cart = document.querySelector('.cart');
-let order = [];
+const close_cart = document.querySelector('.close');
+const details = document.querySelector('.details');
+const order = document.querySelector('.order');
+const close_details = document.querySelector('.close_details');
 let item = "";
 let price = "";
 let list_item = "";
@@ -17,10 +20,17 @@ let quantity = 1;
 
 
 // Functions
-const hello = function() {
+const ham_on = function() {
     ham.classList.toggle("active")
     s_bar.classList.toggle("active");
-    if (getComputedStyle(dim).display=="none") { dim.style.display="block"; }
+    if (details.classList.contains("active") || s_bar.classList.contains("active")) { dim.style.display="block"; }
+    else { dim.style.display="none"; };
+}
+const ham_off = function() {
+    ham.classList.remove("active")
+    s_bar.classList.remove("active");
+    details.classList.remove("active");
+    if (details.classList.contains("active") || s_bar.classList.contains("active")) { dim.style.display="block"; }
     else { dim.style.display="none"; };
 };
 const menu = function() {
@@ -59,15 +69,46 @@ const added_show = function() {
         };
     };
     let cart_item = cart_list.childNodes;
-    if (cart_item.length>0 && !cart.classList.contains("active")) { cart.classList.add("active"); }
-    else if (cart_item.length<1 && cart.classList.contains("active")) { cart.classList.remove("active");}
+    if (cart_item.length>0 && !cart.classList.contains("active")) { 
+        cart.classList.add("active");
+        (cart.childNodes[1]).classList.remove("inactive");
+    }
+    else if (cart_item.length<1 && cart.classList.contains("active")) { 
+        cart.classList.remove("active");
+        (cart.childNodes[1]).classList.add("inactive");
+    }
 };
-
+const empty_cart = function() {
+    let cart_item = cart_list.childNodes;
+    let items_count = cart_item.length;
+    for (k=0; k<dish.length; k++) {
+        dish[k].childNodes[1].classList.remove('bxs-minus-circle');
+        dish[k].childNodes[1].classList.add('bxs-plus-circle');
+    };
+    for (i=0; i<items_count; i++) {
+        cart_list.removeChild(cart_item[0]);
+    };
+    (cart.childNodes[1]).classList.add("inactive");
+    cart.classList.remove("active");
+};
+const order_now = function() {
+    details.classList.add("active");
+    if (details.classList.contains("active") || s_bar.classList.contains("active")) { dim.style.display="block"; }
+    else { dim.style.display="none"; };
+}
+const details_display = function() {
+    details.classList.remove("active");
+    if (details.classList.contains("active") || s_bar.classList.contains("active")) { dim.style.display="block"; }
+    else { dim.style.display="none"; };
+}
 
 // Function Calling
-ham.addEventListener('click', hello);
+ham.addEventListener('click', ham_on);
 loop(section, menu);
-dim.addEventListener('click', hello);
-loop(link, hello);
+dim.addEventListener('click', ham_off);
+loop(link, ham_off);
 assign_add_icons(dish);
 loop(dish, added_show);
+close_cart.addEventListener('click', empty_cart);
+order.addEventListener('click', order_now);
+close_details.addEventListener('click', details_display);
